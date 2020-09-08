5f3ace03dd04687054be73a99c769dcd183969b0
shooter1980 <shooter.adm@gmail.com>
2020-09-07 23:15:43 -0400

    services

---
+++ src/app/services/data.service.ts
@@ -0,0 +1,52 @@
+import{Item} from './item'
+import {Injectable} from '@angular/core';
+import {LogService} from './log.service';
+import { HttpClient, HttpParams} from '@angular/common/http';
+import {Observable, throwError} from 'rxjs';
+import {catchError, map} from 'rxjs/operators';
+
+
+@Injectable()
+export class DataService{
+
+  constructor(private logService: LogService, private http: HttpClient){}
+
+  public getData(): Observable<Item[]> {
+        this.logService.write("get items");
+        return this.http.get<Item[]>('http://localhost:3000/api/items').pipe(map(res => {
+          return res.map(item => {
+            return new Item(
+              item.purchase,
+              item.price,
+              item.count,
+              item._id,
+              );
+          });
+        }));
+  }
+
+  public addItem(purchase: string, price: number, count: number) {
+    if(purchase !=null && price!=null && count!=null) {
+      this.logService.write("add item");
+      let item = new Item(purchase, price, count);
+      const params: HttpParams = new HttpParams()
+      .set('purchase', item.purchase)
+      .set('price', item.price.toString())
+      .set('count', item.count.toString())
+      .set('sum', item.sum.toString());
+      return this.http.get('http://localhost:3000/api/add_item', {params:params}).toPromise();
+    }
+  }
+
+  public delItem(items : Item[]) {
+    console.info("del item");
+    items.forEach(element => {
+      if(element.flag===true){
+        console.info(element._id);
+        const params: HttpParams = new HttpParams()
+          .set('_id', element._id);
+        this.http.get('http://localhost:3000/api/del_item', {params:params}).toPromise();
+      }
+    });
+  }
+}
---
+++ src/app/services/item.ts
@@ -0,0 +1,19 @@
+export class Item {
+    _id? : string;
+	  flag : boolean;
+    purchase: string;
+    price: number;
+    count: number;
+    sum?: number;
+
+    constructor( purchase: string, price: number, count: number, _id? : string) {
+        this._id = _id;
+        this.purchase = purchase;
+        this.price = price;
+        this.count = count;
+		    this.flag = false;
+        this.sum = price * count;
+    }
+
+
+}
