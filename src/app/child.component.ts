a738b559a3fc32ffac43e8d6fc292ca8b74e88b9
shooter1980 <shooter.adm@gmail.com>
2020-09-14 14:02:55 -0400

    reactive form

--- src/app/child.component.ts
+++ src/app/child.component.ts
@@ -1,5 +1,5 @@
 import { DataService } from './services/data.service';
-import { Component, ViewChild} from '@angular/core';
+import { Component} from '@angular/core';
 import {FormGroup, FormControl, Validators, ReactiveFormsModule } from "@angular/forms";
 import{ Item } from './services/item';
 import { ClrForm } from '@clr/angular';
@@ -14,10 +14,12 @@ import { ClrForm } from '@clr/angular';
     price: number ;
     count: number ;
       addForm = new FormGroup({
-        inputControl: new FormControl('', Validators.required),
+        // inputControl: new FormControl('', Validators.required),
+        purchase: new FormControl('', Validators.required),
+        price: new FormControl('', Validators.required),
+        count: new FormControl('', Validators.required),
     });
 
-  @ViewChild(ClrForm, {static: true}) clrForm;
 
   resetForm() {
     this.addForm.reset();
@@ -27,7 +29,7 @@ import { ClrForm } from '@clr/angular';
 
     addItem(purchase: string, price: number, count: number): void {
       if (!this.addForm.invalid) {
-        this.dataService.addItem(purchase, price, count);
+        this.dataService.addItem(this.addForm.get("purchase").value, this.addForm.get("price").value, this.addForm.get("count").value);
       }
     }
 
